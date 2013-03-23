ef097f744ff29b746efbc5673942e958361fc574
Sabarish Sankar <sabarish@chronus.com>
2013-03-23 11:51:09 +0530

    new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupme::Application.config.secret_token = '6f1307a28e83c0702173d70b821572d986bad502a1ec3051421995648e846cdb548d0995e51331242eba379e9d36fc3b05ce700a0d1b9e02cffd38dabc32f310'
